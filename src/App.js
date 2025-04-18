a9d558dbb5ec2dab0ff2b70c9619cd3a3964cf8f
cloudshare360 <cloud.share360@gmail.com>
2025-04-18 16:46:01 +0000

    updated the different header with different params

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import Header from './components/Header'
 function App() {
   return (
     <>
-      <Header />
+      <Header text="Header Text" bgColor="blue" textColor="white" />
+      <Header text="Default with out bgcolor and textcolor" />
+      <Header bgColor="blue" textColor="white" />
+
       <div className='container'>
         <h1>My App</h1>
       </div>

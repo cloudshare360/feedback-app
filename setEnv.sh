c7ba56d787f1966083461c39a6c09399e97030a9
cloud share360 <cloud.share360@gmail.com>
2025-04-18 20:53:15 +0000

    updated
    Signed-off-by: cloud share360 <cloud.share360@gmail.com>

--- setEnv.sh
+++ setEnv.sh
@@ -1,5 +1,7 @@
 # node version 16.13.1 LTS
-$NVM_DIR/nvm install 16.13.1
-$NVM_DIR/nvm use 16.13.1
+#$NVM_DIR/nvm install 16.13.1
+#$NVM_DIR/nvm use 16.13.1
+nvm install 16.13.1 && nvm use 16.13.1
 rm -rf node_modules
-#npm install 
+npm install 
+npm start
